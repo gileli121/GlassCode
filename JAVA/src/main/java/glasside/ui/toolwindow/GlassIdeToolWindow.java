93d7634733080a571475d21ca93cc2c1e52fe42d
Gil Eliyahu <gileli121@gmail.com>
2021-01-04 19:36:31 +0200

    added basic logic to the ui
    ui will now response to events but still not do anything with them

--- JAVA/src/main/java/glasside/ui/toolwindow/GlassIdeToolWindow.java
+++ JAVA/src/main/java/glasside/ui/toolwindow/GlassIdeToolWindow.java
@@ -5,6 +5,10 @@ package glasside.ui.toolwindow;
 import com.intellij.openapi.wm.ToolWindow;
 
 import javax.swing.*;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
 import java.util.Calendar;
 
 public class GlassIdeToolWindow {
@@ -18,9 +22,98 @@ public class GlassIdeToolWindow {
     private JLabel brightnessLabel;
     private JLabel blurTypeLabel;
 
+    private boolean isUiUpdating = false;
+
     public GlassIdeToolWindow(ToolWindow toolWindow) {
+        // TODO: Set the sliders from the settings
+
+        System.out.println(1);
+
+
+        updateUi();
+
+        // Set event listeners
+        opacitySlider.addChangeListener(e -> {
+            if (!isUiUpdating) onOpacitySliderChange(opacitySlider.getValue());
+        });
+
+        brightnessSlider.addChangeListener(e -> {
+            if (!isUiUpdating) onBrightnessSliderChange(brightnessSlider.getValue());
+        });
+
+        blurTypeSlider.addChangeListener(e -> {
+            if (!isUiUpdating) onBlurTypeSliderChange(blurTypeSlider.getValue());
+        });
+
+        enableCheckBox.addItemListener(e -> {
+            if (!isUiUpdating)
+                onEnableCheckBoxChange(e.getStateChange() == ItemEvent.SELECTED);
+        });
+
 
     }
+
+
+    // region On event methods
+    private void onOpacitySliderChange(int level) {
+        setOpacityLabelText(level);
+    }
+
+    private void onBrightnessSliderChange(int level) {
+        setBrightnessLabelText(level);
+    }
+
+    private void onBlurTypeSliderChange(int level) {
+        setBlurTypeLabelText(level);
+    }
+
+    private void onEnableCheckBoxChange(boolean enabled) {
+
+    }
+    // endregion
+
+
+    // region utility methods
+
+    public void updateUi() {
+
+        // TODO: Later it should load it from the UI state object...
+        int opacityLevel = 50;
+        int brightnessLevel = 50;
+        int blurType = 1;
+        boolean isEnabled = true;
+
+        isUiUpdating = true;
+        opacitySlider.setValue(opacityLevel);
+        brightnessSlider.setValue(brightnessLevel);
+        blurTypeSlider.setValue(blurType);
+        enableCheckBox.setEnabled(isEnabled);
+        isUiUpdating = false;
+
+        setOpacityLabelText(opacityLevel);
+        setBrightnessLabelText(brightnessLevel);
+        setBlurTypeLabelText(blurType);
+    }
+
+
+    private void setOpacityLabelText(int level) {
+        opacityLabel.setText(level + "%");
+    }
+
+    private void setBrightnessLabelText(int level) {
+        brightnessLabel.setText(level + "%");
+    }
+
+    private void setBlurTypeLabelText(int type) {
+        switch (type) {
+            case 1 -> blurTypeLabel.setText("Medium");
+            case 2 -> blurTypeLabel.setText("High");
+            default -> blurTypeLabel.setText("None");
+        }
+    }
+
+    // endregion
+
 
 //    private JButton refreshToolWindowButton;
 //    private JButton hideToolWindowButton;
