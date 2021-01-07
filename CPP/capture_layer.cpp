3480b38c9e27585420a23a1969e13e46602c8c3a
Gil Eliyahu <gileli121@gmail.com>
2021-01-07 19:10:34 +0200

    Renderer: fixed bug that window flickering when running the effect on the first time

--- CPP/capture_layer.cpp
+++ CPP/capture_layer.cpp
@@ -6,6 +6,9 @@
 #include <windows.graphics.capture.h>
 #include <windows.graphics.directx.direct3d11.interop.h>
 
+#include <dwmapi.h>
+#pragma comment(lib, "Dwmapi.lib")
+
 #include <DispatcherQueue.h>
 #include <d3d11.h>
 #pragma comment(lib, "D3D11.lib")
@@ -19,7 +22,7 @@
 namespace capture_layer_helpers
 {
 	bool create_capture_item_for_window_internal(const HWND hwnd, IGraphicsCaptureItemInterop* interop_factory,
-		winrt::Windows::Graphics::Capture::GraphicsCaptureItem* item)
+	                                             winrt::Windows::Graphics::Capture::GraphicsCaptureItem* item)
 	{
 		_COM_Outptr_ const auto result = reinterpret_cast<void**>(winrt::put_abi(*item));
 
@@ -38,7 +41,7 @@ namespace capture_layer_helpers
 	}
 
 	inline auto create_capture_item_for_window(const HWND hwnd,
-		winrt::Windows::Graphics::Capture::GraphicsCaptureItem* item)
+	                                           winrt::Windows::Graphics::Capture::GraphicsCaptureItem* item)
 	{
 		const auto activation_factory = winrt::get_activation_factory<
 			winrt::Windows::Graphics::Capture::GraphicsCaptureItem>();
@@ -59,29 +62,29 @@ namespace capture_layer
 	TextureData texture_data;
 
 	// WinRT stuff for the logic that used to capture with Win 10 API
-	winrt::Windows::Graphics::Capture::GraphicsCaptureItem capture_item = { nullptr };
+	winrt::Windows::Graphics::Capture::GraphicsCaptureItem capture_item = {nullptr};
 	winrt::Windows::Graphics::SizeInt32 capture_last_size;
 	winrt::Windows::Graphics::Capture::Direct3D11CaptureFramePool::FrameArrived_revoker frame_arrived_revoker;
 
 
 	// The 3d device stuff that used for the logic to get the captured data as ID3D11Texture2D
-	winrt::Windows::Graphics::Capture::Direct3D11CaptureFramePool frame_pool{ nullptr };
-	winrt::Windows::Graphics::Capture::GraphicsCaptureSession capture_session{ nullptr };
+	winrt::Windows::Graphics::Capture::Direct3D11CaptureFramePool frame_pool{nullptr};
+	winrt::Windows::Graphics::Capture::GraphicsCaptureSession capture_session{nullptr};
 
 	bool new_frame = false;
 	bool first_frame = true;
 	bool is_closed = true;
 	clock_t resize_frame_timer = 0;
 
-	
+
 	//bool capturing = false; // Moved to global
 
 	bool init()
 	{
 		init_apartment(winrt::apartment_type::single_threaded);
 
-		winrt::Windows::System::DispatcherQueueController controller{ nullptr };
-		const DispatcherQueueOptions options{ sizeof(DispatcherQueueOptions), DQTYPE_THREAD_CURRENT, DQTAT_COM_STA };
+		winrt::Windows::System::DispatcherQueueController controller{nullptr};
+		const DispatcherQueueOptions options{sizeof(DispatcherQueueOptions), DQTYPE_THREAD_CURRENT, DQTAT_COM_STA};
 		const auto res = CreateDispatcherQueueController(
 			options, reinterpret_cast<ABI::Windows::System::IDispatcherQueueController**>(winrt::put_abi(controller)));
 		if (res != S_OK)
@@ -116,7 +119,7 @@ namespace capture_layer
 
 		new_size = false;
 		if (frame_content_size.Width != capture_last_size.Width || frame_content_size.Height != capture_last_size.Height
-			)
+		)
 		{
 			if (first_frame)
 			{
@@ -128,7 +131,6 @@ namespace capture_layer
 				resize_frame_timer = clock();
 				capture_last_size = frame_content_size;
 			}
-
 		}
 		else if (resize_frame_timer && clock() - resize_frame_timer > 250)
 		{
@@ -136,14 +138,13 @@ namespace capture_layer
 			new_size = true;
 		}
 
-		
 
 		if (new_size)
 		{
 			// The thing we have been capturing has changed size.
 			// We need to resize our swap chain first, then blit the pixels.
 			// After we do that, retire the frame and then recreate our frame pool.
-			
+
 			graphic_device::resize_swap_chain(capture_last_size.Width, capture_last_size.Height);
 			texture_data.x_size = capture_last_size.Width;
 			texture_data.y_size = capture_last_size.Height;
@@ -163,8 +164,8 @@ namespace capture_layer
 		{
 			if (!create_capture_item_for_window(target_hwnd, &capture_item))
 				return;
-			
-			
+
+
 			frame_pool.Recreate(
 				graphic_device::device,
 				DirectXPixelFormat::B8G8R8A8UIntNormalized,
@@ -180,7 +181,6 @@ namespace capture_layer
 
 	bool create_layer()
 	{
-		using namespace winrt;
 		using namespace winrt::Windows::Graphics::Capture;
 		using namespace winrt::Windows::Graphics::DirectX;
 		using namespace capture_layer_helpers;
@@ -194,9 +194,18 @@ namespace capture_layer
 			return false;
 		}
 
-		const auto size = capture_item.Size();
+		RECT target_rect = {0};
+		if (DwmGetWindowAttribute(target_hwnd, DWMWA_EXTENDED_FRAME_BOUNDS, &target_rect, sizeof(RECT)) != S_OK)
+		{
+			std::cout << "DwmGetWindowAttribute(*) failed while trying to get the size of the capture frame" <<
+				std::endl;
+			return false;
+		}
 
-		// Create framepool, define pixel format (DXGI_FORMAT_B8G8R8A8_UNORM), and frame size. 
+		const winrt::Windows::Graphics::SizeInt32 size = {
+			target_rect.right - target_rect.left, target_rect.bottom - target_rect.top
+		};
+
 
 		frame_pool = Direct3D11CaptureFramePool::Create(
 			graphic_device::device,
@@ -206,9 +215,8 @@ namespace capture_layer
 
 		capture_session = frame_pool.CreateCaptureSession(capture_item);
 
-
 		capture_last_size = size;
-		frame_arrived_revoker = frame_pool.FrameArrived(auto_revoke, &callback_on_frame_arrived);
+		frame_arrived_revoker = frame_pool.FrameArrived(winrt::auto_revoke, &callback_on_frame_arrived);
 
 		is_closed = false;
 		return true;
@@ -234,11 +242,10 @@ namespace capture_layer
 
 	void dispose()
 	{
-
 		// stop_capture_session();
 
 #if 1 // I gave up.. there is no way to dispose this shit. It will just crash... 
-		capture_last_size = { 0 };
+		capture_last_size = {0};
 
 		if (!is_closed)
 		{
@@ -254,7 +261,7 @@ namespace capture_layer
 		frame_pool = nullptr;
 		capture_session = nullptr;
 		capture_item = nullptr;
-		texture_data = { nullptr };
+		texture_data = {nullptr};
 		capturing = false;
 		new_frame = false;
 		first_frame = true;
