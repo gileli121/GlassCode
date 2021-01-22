8d326603bb8327719d0f6f2124edf82de79e8093
Gil Eliyahu <gileli121@gmail.com>
2021-01-22 02:38:51 +0200

    Plugin: Change default opacity values and fix some bug

--- JAVA/src/main/java/glasscode/GlassCodeStorage.java
+++ JAVA/src/main/java/glasscode/GlassCodeStorage.java
@@ -15,10 +15,10 @@ import org.jetbrains.annotations.Nullable;
 )
 public class GlassCodeStorage implements PersistentStateComponent<GlassCodeStorage> {
 
-    private static final int DEFAULT_OPACITY = 70;
-    private static final int DEFAULT_BRIGHTNESS = 70;
+    private static final int DEFAULT_OPACITY = 80;
+    private static final int DEFAULT_BRIGHTNESS = 60;
     private static final int DEFAULT_BLUR_TYPE = 0;
-    private static final int DEFAULT_TEXT_EXTRA_BRIGHTNESS = 70;
+    private static final int DEFAULT_TEXT_EXTRA_BRIGHTNESS = 0;
     private static final boolean DEFAULT_CUDA_ENABLED = true;
     private static final boolean DEFAULT_IS_GLASS_ENABLED = false;
     private static final boolean DEFAULT_USE_CONTRAST_THEME = true;
